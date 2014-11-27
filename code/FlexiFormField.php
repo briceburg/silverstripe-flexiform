0d04462ee1c3d97d0626b05fca6bd9a8d065613e
Brice Burgess <nesta@iceburg.net>
2014-11-27 01:08:09 -0500

    Add ability to autogenerate form fields under /dev/build

--- code/FlexiFormField.php
+++ code/FlexiFormField.php
@@ -9,9 +9,13 @@ class FlexiFormField extends DataObject
 
     protected $field_description = 'Override Me';
 
+    // used to automatically generate fields during /dev/build
+    protected $field_definition = null;
+
     private static $db = array(
         'FieldName' => 'Varchar(16)',
-        'FieldDefaultValue' => 'Varchar'
+        'FieldDefaultValue' => 'Varchar',
+        'Readonly' => 'Boolean'
     );
 
     private static $has_many = array(
@@ -50,24 +54,25 @@ class FlexiFormField extends DataObject
         $field->setTitle('Default Value');
         $field->description = 'Optional. Will prepopulate the field with this value.';
 
-        $field = new LiteralField('Description', "<strong>{$this->field_label} Field &mdash;</strong> {$this->field_description} <hr />");
+        $field = new LiteralField('Description',
+            "<strong>{$this->field_label} Field &mdash;</strong> {$this->field_description} <hr />");
         $fields->addFieldToTab('Root.Main', $field, 'FieldName');
 
         return $fields;
     }
 
-    public function validate() {
+    public function validate()
+    {
         $result = parent::validate();
 
-        if($result->valid()) {
-            if(empty($this->FieldName)) {
+        if ($result->valid()) {
+            if (empty($this->FieldName)) {
                 $result->error('Name cannot be blank');
             }
         }
 
         return $result;
     }
-
 
     // override this method for custom behavior
     public function getFormField($name, $value = null)
@@ -97,20 +102,60 @@ class FlexiFormField extends DataObject
         return '-';
     }
 
-    public function getTitle() {
-        return "{$this->FieldName} ({$this->field_label})";
+    public function getTitle()
+    {
+        $system = ($this->SystemField) ? '*' : '';
+        return "{$this->FieldName} ({$this->field_label})$system";
     }
 
-    public function getDefaultValue(){
+    public function getDefaultValue()
+    {
         $value = $this->getField('DefaultValue');
         $name = $this->getField('Name');
         // && empty($name) ensures DefaultValue is set only once, and subsequent allows empty values.
         return (empty($value) && empty($name)) ? $this->FieldDefaultValue : $value;
     }
 
-    public function getName(){
+    public function getName()
+    {
         $value = $this->getField('Name');
         return (empty($value)) ? $this->FieldName : $value;
     }
 
+    public function getFlexiFieldDefinition()
+    {
+        return $this->field_definition;
+    }
+
+    public function setFlexiFieldDefinition(Array $flexi_field_definition)
+    {
+        return $this->field_definition = $flexi_field_definition;
+    }
+
+    public function requireDefaultRecords()
+    {
+        // if this field has a definition, attempt to create it
+        $definition = $this->getFlexiFieldDefinition();
+
+        if ($definition !== null) {
+
+            $filter = array(
+                'FieldName' => $definition['Name'],
+                'Readonly' => (isset($definition['Readonly']) && $definition['Readonly'])
+            );
+
+            // only create field if it's name doesn't yet exist
+            if (! FlexiFormField::get()->filter($filter)->first()) {
+
+                if ($field = FlexiFormUtil::CreateFlexiField($definition)) {
+                    $readonly = ($field->Readonly) ? 'Readonly' : 'Normal';
+                    DB::alteration_message(
+                        "flexiforms - Created $readonly $this->className} named `{$field->FieldName}`.",
+                        "created");
+                }
+            }
+        }
+
+        return parent::requireDefaultRecords();
+    }
 }
