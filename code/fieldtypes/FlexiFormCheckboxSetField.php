14027179f7d5b8ad52a1463713a7a98644f9a3bd
Brice Burgess <nesta@iceburg.net>
2014-11-26 23:01:07 -0500

    fix radio and checkbox set types

--- code/fieldtypes/FlexiFormCheckboxSetField.php
+++ code/fieldtypes/FlexiFormCheckboxSetField.php
@@ -1,9 +1,8 @@
 <?php
 
-class FlexiFormCheckboxSetField extends FlexiFormField {
+class FlexiFormCheckboxSetField extends FlexiFormOptionField {
     protected $field_class = 'CheckboxSetField';
     protected $field_label = 'Checkbox Set';
     protected $field_description = 'Displays supplied options to choose one or more from.';
-    protected $field_has_options = true;
 
 }
--- code/fieldtypes/FlexiFormRadioField.php
+++ code/fieldtypes/FlexiFormRadioField.php
@@ -1,9 +1,8 @@
 <?php
 
-class FlexiFormRadioField extends FlexiFormField {
+class FlexiFormRadioField extends FlexiFormOptionField {
     protected $field_class = 'OptionsetField';
     protected $field_label = 'Checkbox';
     protected $field_description = 'Displays supplied options to choose one from.';
-    protected $field_has_options = true;
 
 }
