acb94d50f498499bd2cd0493c20786f531d7767c
Brice Burgess <nesta@iceburg.net>
2014-11-30 22:56:44 -0500

    better handle default value selcetion for checkbox fields

--- code/model/fieldtypes/FlexiFormCheckboxField.php
+++ code/model/fieldtypes/FlexiFormCheckboxField.php
@@ -5,4 +5,13 @@ class FlexiFormCheckboxField extends FlexiFormField {
     protected $field_label = 'Checkbox';
     protected $field_description = 'Displays a checkbox field. Yes/No Value.';
 
+    public function getDefaultValueFormField($field_name = 'FieldDefaultValue')
+    {
+        $field = new DropdownField($field_name, 'Default Value', array(
+            0 => 'unchecked',
+            1 => 'checked'
+        ));
+        return $field;
+    }
+
 }
